61728f17a98a258a81a58e1666202e50ec25d291
Richard Cox <code@swrm.io>
2024-08-14 23:51:18 -0700

    Feat: Add Routine support

--- routines.go
+++ routines.go
@@ -1,1 +1,32 @@
 package hevy
+
+// A response for fetching a list of workouts
+type routineResponse struct {
+	paginatedResults
+	Routines []Routine `json:"routines"`
+}
+
+// Routines gets all routines.
+func (c Client) Routines() ([]Routine, error) {
+	routines := []Routine{}
+
+	page := 1
+	pageCount := 10
+	for {
+		url := c.constructURL("routines", page, pageCount)
+		result := routineResponse{}
+		err := c.get(url, &result)
+		if err != nil {
+			return nil, err
+		}
+
+		routines = append(routines, result.Routines...)
+
+		if result.Page == result.PageCount {
+			break
+		}
+		page++
+	}
+
+	return routines, nil
+}
---
+++ routines_test.go
@@ -0,0 +1,37 @@
+package hevy_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/swrm-io/go-hevy"
+)
+
+func TestRoutine(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		switch req.URL.Path {
+		case "/v1/routines":
+			page := req.URL.Query().Get("page")
+
+			file := fmt.Sprintf("testdata/responses/routine-%s.json", page)
+			data, err := os.ReadFile(file)
+			assert.NoError(t, err)
+			res.Write(data)
+		}
+	}))
+	defer srv.Close()
+
+	client := hevy.NewClient("my-fake-api-key")
+	client.ApiURL = srv.URL
+
+	t.Run("Test Paginated Routines", func(t *testing.T) {
+		routines, err := client.Routines()
+		assert.NoError(t, err)
+		assert.NotEmpty(t, routines)
+		assert.Len(t, routines, 3)
+	})
+}
